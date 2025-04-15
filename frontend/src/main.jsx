c83d81af5179e7faebda5d7aae139d516e165915
Andrew Fynaardt <andrew.fynaardt@student.cune.edu>
2025-04-15 13:55:27 -0500

    Added new packages & began reminders List page.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const client = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Hello World</div>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
