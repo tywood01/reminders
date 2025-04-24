59d3fa8181d6e384a9df0d8739be225af52baad7
Andrew Fynaardt <andrew.fynaardt@student.cune.edu>
2025-04-24 14:13:56 -0500

    Implemented datetime and recurring fields on the model using the calender interface and a drop down

--- backend/reminders/models.py
+++ backend/reminders/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Reminder(models.Model):
+    class recurChoice(models.TextChoices):
+        NO = ("no", "No")
+        DAILY = ("daily", "Daily")
+        WEEKLY = ("weekly", "Weekly")
     name = models.CharField("name", max_length=255, blank=False, default="")
     body = models.TextField("body", blank=True)
-    """time = models.TimeField("time", blank=False)
-    recurring = models.BooleanField("recurring", default=False)
-    date = models.DateField("date", blank=False) """
+    recurring = models.CharField("recurring", choices=recurChoice, max_length=255, default="no") # no, daily, weekly
+    datetime = models.DateTimeField("datetime", blank=False, default=timezone.now)
 
 
 # Maybe create a model for lists of reminders
--- frontend/src/pages/ReminderAdd.jsx
+++ frontend/src/pages/ReminderAdd.jsx
@@ -1,24 +1,31 @@
 /**
  * ReminderAdd.jsx
- * Reagan Zierke, Tytus Woodburn
+ * Reagan Zierke, Tytus Woodburn, Andrew Fynaardt
  * 
  * Page for adding a new reminder
  */
 
 import React, { useState } from "react";
-import { useReminderCreate } from "../hooks/useReminder";
 import { createReminder } from "../data/reminders";
 import { useNavigate } from "react-router";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { queryOptions, useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function ReminderCreate() {
  	// We cannot invoke this call inside of a conditional
 	// because of the laws of hooks for React.
 	const navigate = useNavigate();
 
+    const recurOptions = [
+        { name: "No", value: "no"},
+        { name: "Daily", value: "daily"},
+        { name: "Weekly", value: "weekly"},
+    ]   
+
 	const queryClient = useQueryClient();
 	const [name, setName] = useState("");
     const [body, setBody] = useState("");
+    const [datetime, setDatetime] = useState("");
+    const [recur, setRecur] = useState("")
 	// const { name, setName, loading, error, successful, createTask } =
 	// 	useTaskCreate();
 	const { isPending, isError, mutate } = useMutation({
@@ -68,6 +75,20 @@ export function ReminderCreate() {
 					value={body}
 					onChange={(e) => setBody(e.target.value)}
 				/>
+                <h2>Datetime</h2>
+                <input
+					type="datetime-local"
+					value={datetime}
+					onChange={(e) => setDatetime(e.target.value)}
+				/>
+                <h2>Recurring?</h2>
+                <select onChange={(e) => setRecur(e.target.value)}>
+                    {recurOptions.map((option) => (
+                        <option key={option.value} value={option.value}>
+                          {option.name}
+                        </option>
+                    ))}
+                </select>
                 <br/>
                 <br/>
 				<button type="submit">Create</button>
